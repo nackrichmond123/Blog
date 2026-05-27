2e7d135a1d44852dd8dd24e3c217eb2b28afd2a2
Nack Richmond <nackrichmond123@gmail.com>
2026-05-27 15:05:40 +0000

    1:Completed the popular and latest blogs.  2:Started the about us section and almost done

---
+++ index.js
@@ -0,0 +1,23 @@
+
+// const blog1 = {
+//     name : 'MACHINE LEARNING AND AI',
+//     image (){
+//         document.getElementById('image1').src = 'Images/try.png';
+//     }
+// };
+
+// blog1.image();
+
+// Create Image element
+// let Image = document.createElement('img');
+// Image.src = "Images/try.png";
+
+// document.getElementById('image1') = Image;
+
+//  document.getElementById('image1').src = 'Images/try.png';
+// blog1.image();
+// document.getElementById('l-name1').innerHTML = blog1.name;
+
+
+// let date = new Date;
+// document.getElementById('l-date-time1').innerHTML = date.toDateString();
